425b6782eaf029164b3d9beb8600f97e61fbe56c
Vladislav Bezrodny <bezrodnyi7@gmail.com>
2026-02-27 16:46:49 +0500

    lab7

--- mysite/articles/models.py
+++ mysite/articles/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    thumbnail = models.ImageField(upload_to="images/", blank=True)
 
     def __str__(self):
         return self.title
